85af1487394aee97973853ed78a6dbaa832e24b5
LeSitar <lenasitars@gmail.com>
2020-09-08 14:56:02 +0200

    Java Classes/ Calculator

---
+++ 20200908_Calculator/src/de/telran/application/CalculatorApplication.java
@@ -0,0 +1,18 @@
+package de.telran.application;
+
+import de.telran.data.Calculator;
+import de.telran.data.UserInteraction;
+
+import static de.telran.data.UserInteraction.getParameter;
+
+public class CalculatorApplication {
+    public static void main(String[] args) {
+
+        double a = getParameter();
+        double b = getParameter();
+        char operation = UserInteraction.operation();
+        Calculator.calculation(a,b,operation);
+        UserInteraction.closeScanner();
+
+    }
+}
---
+++ 20200908_Calculator/src/de/telran/data/UserInteraction.java
@@ -0,0 +1,38 @@
+package de.telran.data;
+
+import java.util.Scanner;
+
+public class UserInteraction {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+   public static double getParameter(){
+       System.out.println("Enter number, please");
+        return scanner.nextDouble();
+    }
+
+    // double a = getParameter();
+
+    public static char operation(){
+        System.out.println("Enter operation symbol, please");
+        char operation = scanner.next().charAt(0);
+        while (operation != '+' && operation != '-' && operation !='*' && operation != '/' ){
+            System.out.println("wrong operation symbol, enter new please");
+            operation = scanner.next().charAt(0);
+        }
+        return operation;
+    }
+
+    static double getRightDelimiter(double delimiter){           //use only in class in same package
+       while (delimiter==0){
+           System.out.println("Error. Enter another number");
+           delimiter = scanner.nextDouble();
+       }
+       return delimiter;
+    }
+
+    public static void closeScanner(){
+       scanner.close();
+    }
+
+}
