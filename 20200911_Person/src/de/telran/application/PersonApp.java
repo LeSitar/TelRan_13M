6e45b4ce1862b406148b892dbf3910dc706440e7
LeSitar <lenasitars@gmail.com>
2020-09-15 11:28:25 +0200

    Project Person

--- 20200911_Person/src/de/telran/application/PersonApp.java
+++ 20200911_Person/src/de/telran/application/PersonApp.java
@@ -1,5 +1,6 @@
 package de.telran.application;
 
+import de.telran.data.Address;
 import de.telran.data.Person;
 
 import java.util.Arrays;
@@ -8,15 +9,38 @@ public class PersonApp {
 
     public static void main(String[] args) {
 
-        Person person1 = new Person("Vasya", 25);
+        Address address1 = new Address("Berlin", "Kudamm", 24);
+        Person person1 = new Person("Vasya", 2, address1);
         person1.setName("Vasiliy");
-        Person person2 = new Person("Alex", 24);
-        Person person3 = new Person ("Katya", 21);
-        Person person4 = new Person ("Masha", 23);
+        Person person2 = new Person("Alex", 24, new Address("London", "Becker", 15 ));
+        Person person3 = new Person ("Katya", 21, new Address("Moscow", "Puschkinskaya", 16));
+        Person person4 = new Person ("Masha", 23, address1);
 
-        Person[] group = {person1,person2,person3,person4};
+        Person person5 = new Person ("Alex", 28, new Address("Paris", "MonMarteu", 6));
 
+        Address address2 = person2.getAddress();
+
+
+        Person[] group = {person1,person2,person3,person4, person5};
+
+        //Person.displayPersons(group);
+        Person.sortPersonByAge(group);
         Person.displayPersons(group);
+        System.out.println();
+        //Person.sortPersonByAge2(group);
+        //Person.displayPersons(group);
+
+       /* String str1 = "Kolya";
+        String str2 = "Vasya";
+
+        System.out.println(str1.compareTo(str2));*/
+        System.out.println();
+      // Person.sortPersonByName(group);
+      // Person.displayPersons(group);
+
+       //Person foundPersona = Person.findPersonByAddress(group, address1);
+        System.out.println(Person.findPersonByAddress(group, new Address("Belin", "Kudamm",7)));
+
 
         //System.out.println(Arrays.toString(group));
     }
--- 20200911_Person/src/de/telran/data/Person.java
+++ 20200911_Person/src/de/telran/data/Person.java
@@ -3,10 +3,19 @@ package de.telran.data;
 public class Person {
     private String name;
     private int age;
+    private double height;
+    private Address address;
 
-    public Person(String name, int age){
+
+
+    public Person(String name, int age, Address address ){
         this.name = name;
         this.age = age;
+        this.address = address;
+    }
+
+    public double getHeight() {
+        return height;
     }
 
     public String getName() {
@@ -29,11 +38,41 @@ public class Person {
 
     }
 
+    public Address getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (getAge() != person.getAge()) return false;
+        if (Double.compare(person.getHeight(), getHeight()) != 0) return false;
+        if (!getName().equals(person.getName())) return false;
+        return getAddress().equals(person.getAddress());
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = getName().hashCode();
+        result = 31 * result + getAge();
+        temp = Double.doubleToLongBits(getHeight());
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + getAddress().hashCode();
+        return result;
+    }
 
     @Override
     public String toString() {
         return  "Name: " + name +
-                ", age: " + age;
+                ", age: " + age
+                + ", height: " + height + '\n' + address;
     }
 
     public static void displayPersons(Person[] persons){
@@ -42,10 +81,107 @@ public class Person {
         }
     }
 
-    public static Person getOldestPerson(Person[] persons){
-        return null;
+  /*  public static int findMax(int[] numbers){ //{1,2,3,4,5}
+        int max = 0;
+        for (int i = 0; i <numbers.length ; i++) {
+            if(numbers[i]>max){
+                max = numbers[i];
+            }
+        }
+        return max;
+    }*/
+
+
+    public static Person getOldestPerson(Person[] persons){  //{Vasya, Petya, Olya, Masha}
+        Person oldestPerson = persons[0];
+        for (int i = 1; i <persons.length ; i++) {
+            if(persons[i].age>oldestPerson.age){
+                oldestPerson = persons[i];
+            }
+        }
+        return oldestPerson;
     }
 
     public static void displayOldestPerson(Person[] persons){
     }
+
+    // 6>9
+    // p1>p2 ?
+
+    private static int comparePersonByAge(Person p1, Person p2){
+        if(p1.age>p2.age)
+            return 1;
+        else if(p1.age<p2.age)
+                return -1;
+        return 0;
+    }
+
+    private int compareToPersonByAge(Person person){
+        if(this.age>person.age)
+            return 1;
+        else if(this.age<person.age)
+            return -1;
+        return 0;
+    }
+
+    public static void sortPersonByAge(Person[] persons){
+        for (int i = 0; i <persons.length-1 ; i++){
+            for (int j = 0; j <(persons.length-1)-i ; j++) {
+                if(comparePersonByAge(persons[j], persons[j+1])<0){
+                    Person temp = persons[j+1];
+                    persons[j+1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+        }
+    }
+
+    public static void sortPersonByAge2(Person[] persons){
+        for (int i = 0; i <persons.length-1 ; i++) {
+            for (int j = 0; j <(persons.length-1) -i ; j++) {
+                if(persons[j].compareToPersonByAge(persons[j+1])>0){
+                    Person temp = persons[j+1];
+                    persons[j+1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+
+        }
+    }
+
+    public static void sortPersonByName(Person[] persons){
+        for (int i = 0; i <persons.length-1; i++) {
+            for (int j = 0; j < (persons.length-1) -i; j++) {
+                if(persons[j].name.compareTo(persons[j+1].name)>0){
+                    Person temp = persons[j+1];
+                    persons[j+1] = persons[j];
+                    persons[j] = temp;
+                }
+            }
+
+        }
+    }
+
+    public static Person findPersonByAddress(Person[] persons, Address address){
+        for (Person p:persons) {
+           if(address.equals(p.address)){
+               return p;
+           }
+        }
+       // System.out.println("Person not found");
+        return null;
+    }
+
+    public static void displayPersonByAddress(Person[] persons, Address address){
+        boolean flag=false;
+        for (Person p:persons) {
+            if(address.equals(p.address)){
+                System.out.println(p);
+                flag = true;
+            }
+        }
+        if(!flag)
+        System.out.println("Person not found");
+    }
+
 }
