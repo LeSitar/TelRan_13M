fb7c6c166f62641da02f122c54c29e35bb930af3
LeSitar <lenasitars@gmail.com>
2020-09-29 15:48:57 +0200

    date validator

---
+++ 20200929_DateValidator/src/de/telran/Main.java
@@ -0,0 +1,8 @@
+package de.telran;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DataValidator.dateValidator(2019, 7 , 28));
+        System.out.println(DataValidator.dateValidator(2020, 2 , 29));
+    }
+}
