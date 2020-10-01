9fff0c509befd1577c3f18fbc1ca2eff90b69fa2
LeSitar <lenasitars@gmail.com>
2020-10-01 16:53:32 +0200

    pattern Template method

---
+++ 20201001_Plant/src/de/telran/controller/PlantApp.java
@@ -0,0 +1,49 @@
+package de.telran.controller;
+
+import de.telran.data.Flower;
+import de.telran.data.Plant;
+import de.telran.data.Tree;
+
+public class PlantApp {
+    public static void main(String[] args) {
+        Plant tree1 = new Tree("Baobab", 0,0);
+        Plant tree2 = new Tree("Oak", 1, 100);
+
+        Plant flower1 = new Flower("Tulip", 0,0);
+        Plant flower2 = new Flower("Rose", 1, 2);
+
+       // tree1.doYear();
+       // growPlantForPeriodOfYears(tree1,3);
+
+       // tree2.growPlantForPeriodOfYears(2);
+
+        Plant plants[] = {tree2, flower1};
+
+        growPlantsForPeriodOfYears(plants,2);
+        displayArray(plants);
+
+    }
+
+    private static void growPlantForPeriodOfYears(Plant plant, int years){
+        for (int i = 0; i <years ; i++) {
+            plant.doYear();
+            System.out.println("---------------------------------------");
+        }
+    }
+
+    private static void growPlantsForPeriodOfYears(Plant[] plants, int years){
+        for (int i = 0; i <years ; i++) {
+            for (Plant p:plants) {
+                p.doYear();
+                System.out.println();
+            }
+        }
+    }
+
+    public static void displayArray(Object[] objects){
+        for (Object o: objects )
+            System.out.println(o);
+    }
+
+
+}
